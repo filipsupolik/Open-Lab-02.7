40aed3d9010cb10e23fee79faddf55afdf444309
filip šupolík <filipsupolik25@gmail.com>
2019-10-18 06:23:42 +0200

    completed milk and cookies

--- Open-Lab--02.7/Program.cs
+++ Open-Lab--02.7/Program.cs
@@ -4,9 +4,27 @@ namespace Open_Lab__02._7
 {
     class Program
     {
+        public static bool IsTimeForMilkAndCookies(int year, int month, int day)
+           {
+            return day == 24 && month == 12;
+           }
+
         static void Main(string[] args)
         {
+            int day;
+            int month;
+            int year;
 
+            Console.WriteLine("Type day:");
+            day = int.Parse(Console.ReadLine());
+
+            Console.WriteLine("Type month:");
+            month = int.Parse(Console.ReadLine());
+
+            Console.WriteLine("Type year:");
+            year = int.Parse(Console.ReadLine());
+
+            Console.WriteLine(day, month, year);
         }
     }
 }
